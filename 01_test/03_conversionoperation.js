a4737c666dc476865ded9fc8d1f617ac728f19e1
Harshad Gaikwad <harshadg041@gmail.com>
2025-03-12 18:26:48 +0000

    conversion of datatypes

---
+++ 01_test/03_conversionoperation.js
@@ -0,0 +1,20 @@
+// let score ="33aa"
+// console.log(typeof score)
+
+// let valueinnumber = Number(score)
+// console.log( typeof valueinnumber);
+// console.log(valueinnumber);
+
+let isLoggedIn = 1
+let booleannumber = Boolean(isLoggedIn)
+console.log(booleannumber);
+//1=> true
+//0 => false
+// "" => false
+// "harshad" => true
+
+let somenumber= 34
+let stringnumber=String(somenumber)
+
+console.log(stringnumber);
+
