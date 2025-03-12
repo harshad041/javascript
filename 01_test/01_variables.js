5c45e43bddc86e9ae7a1efaa392f41732c7f770f
Harshad Gaikwad <harshadg041@gmail.com>
2025-03-12 16:22:08 +0000

    datatypes

---
+++ 01_test/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=1
+let accountEmail="harshadg041@gmail.com"
+var accountPassword="12345"
+accountCity="city"
+let accountState;
+
+accountEmail="harsh@gmail.com"
+accountPassword="54321"
+accountCity="pune"
+
+
+/*
+prefer not to use var
+becauase of issue in block scope and functional scoope
+*/
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ 01_test/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3+3)  we are using node.js not web browser
+console.log(4+5)
+console.log("harshad")
+
+let name="harshadd" //String
+let age=18 //integer
+let isLoggedIn = true //boolean
+
+//number = 2 to the power 53
+//bigInt 
+//String => alwys write in double quates
+//boolean => true/false
+//null => standalone value
+//undefined => 
+    //symbol => unique
+
+//object
+
+console.log(typeof null) // type is object
+console.log(typeof undefined); //type is undefine
+
